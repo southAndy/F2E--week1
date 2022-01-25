1e24893d118d0b56d131d834a8f261bbc03e5ee2
AT <zatchbell.1206@gmail.com>
2022-01-25 21:24:27 +0800

    refactor(main):seperate modules

---
+++ f2e-week1/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>
+      台灣走走
+      <img src="" alt="" />
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+<style lang="scss" scoped>
+.header {
+  color: red;
+}
+</style>
