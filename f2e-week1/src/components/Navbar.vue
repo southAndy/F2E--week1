07416459770f3e170ec57de0b9273e71c4c91ac4
AT <zatchbell.1206@gmail.com>
2022-01-25 21:57:34 +0800

    refactor(component):seperate components

---
+++ f2e-week1/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1 class="logo_container">
+      <img src="##" alt="" />
+      台灣向前走
+    </h1>
+  </header>
+</template>
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+<style lang="scss" scoped>
+.logo_container {
+  color: red;
+}
+</style>
