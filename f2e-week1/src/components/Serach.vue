e46762366616abd99478dcdfa362f27b603c7603
AT <zatchbell.1206@gmail.com>
2022-01-26 20:35:04 +0800

    feat(Serach):adding mobile's layout

---
+++ f2e-week1/src/components/Serach.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="serach">
+    <select name="" id="" class="select">
+      <option value="">探索景點</option>
+      <option value="">節慶活動</option>
+      <option value="">品嚐美食</option>
+    </select>
+    <input
+      class="serach_input"
+      type="text"
+      placeholder="你想去哪？請輸入關鍵字"
+    />
+    <a href="##" class="serach_click">
+      <img src="@/assets/image/search.svg" alt="放大鏡" />
+      <span>搜尋</span>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: "Serach",
+};
+</script>
+<style lang="scss" scoped>
+%serach_input {
+  width: 90%;
+  height: 50px;
+  padding: 15px 11px;
+}
+.serach {
+  .select {
+    @extend %serach_input;
+  }
+
+  &_input {
+    @extend %serach_input;
+  }
+}
+</style>
