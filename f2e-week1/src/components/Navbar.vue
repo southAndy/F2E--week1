eb1d5a0ea345d57738f5b9d9edc3adae74dd0df2
AT <zatchbell.1206@gmail.com>
2022-01-26 12:20:03 +0800

    feat(Navbar):create Navbaring

--- f2e-week1/src/components/Navbar.vue
+++ f2e-week1/src/components/Navbar.vue
@@ -1,9 +1,18 @@
 <template>
   <header>
     <h1 class="logo_container">
-      <img src="##" alt="" />
-      台灣向前走
+      <img
+        src="@/assets/image/ScenicSpotPicture.png"
+        alt="網站logo"
+        class="logo"
+      />
+      台灣走走
     </h1>
+    <nav class="navigation">
+      <a href="##" class="navigation_link">探索景點</a>
+      <a href="##" class="navigation_link">節慶活動</a>
+      <a href="##" class="navigation_link">品嚐美食</a>
+    </nav>
   </header>
 </template>
 <script>
@@ -12,7 +21,24 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.logo_container {
-  color: red;
+$navbar-link: #646464;
+$title-size: 20px;
+header {
+  .logo_container {
+    font-size: $title-size;
+    .logo {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .navigation {
+    display: flex;
+
+    &_link {
+      text-decoration: none;
+      margin-left: 20px;
+      color: $navbar-link;
+    }
+  }
 }
 </style>
