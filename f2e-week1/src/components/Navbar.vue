15888cff90e59f7fea4065a1b22dd2dd9d0af3ea
AT <zatchbell.1206@gmail.com>
2022-01-26 19:54:14 +0800

    feat(Navbar):adding mobile's layout,and drop feature

--- f2e-week1/src/components/Navbar.vue
+++ f2e-week1/src/components/Navbar.vue
@@ -1,14 +1,15 @@
 <template>
   <header>
     <h1 class="logo_container">
-      <img
-        src="@/assets/image/ScenicSpotPicture.png"
-        alt="網站logo"
-        class="logo"
-      />
+      <a href="##">
+        <img src="@/assets/image/icon.svg" alt="網站logo" class="logo" />
+      </a>
       台灣走走
     </h1>
-    <nav class="navigation">
+    <div class="toggle" @click="dropMenu()">
+      <img src="@/assets/image/menu.svg" alt="toggle button" />
+    </div>
+    <nav :class="['navigation', { drop: isDropNavbar }]">
       <a href="##" class="navigation_link">探索景點</a>
       <a href="##" class="navigation_link">節慶活動</a>
       <a href="##" class="navigation_link">品嚐美食</a>
@@ -18,26 +19,96 @@
 <script>
 export default {
   name: "Navbar",
+  data() {
+    return {
+      isDropNavbar: true,
+    };
+  },
+  methods: {
+    dropMenu() {
+      console.log("true");
+      this.isDropNavbar = !this.isDropNavbar;
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
 $navbar-link: #646464;
 $title-size: 20px;
+
+//breakpoints
+$media-mobile: 576px;
+$media-tablet: 768px;
+$media-desktop: 1024px;
+$media-bgScreen: 1440px;
+
+@mixin mobile {
+  @media (min-width: $media-mobile) {
+    @content;
+  }
+}
+
+@mixin tablet {
+  @media (min-width: $media-tablet) {
+    @content;
+  }
+}
+
+@mixin desktop {
+  @media (min-width: $media-desktop) {
+    @content;
+  }
+}
+
+@mixin bgScreen {
+  @media (min-width: $media-bgScreen) {
+    @content;
+  }
+}
+
 header {
+  display: flex;
+  justify-content: center;
+
+  position: relative;
   .logo_container {
+    flex: 1;
     font-size: $title-size;
+    a {
+      display: inline-block;
+    }
     .logo {
       width: 100%;
       height: 100%;
     }
   }
+  .drop {
+    display: none !important;
+  }
+  .toggle {
+    @include tablet {
+    }
+  }
+
   .navigation {
     display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 60px;
+    bottom: 0;
+    right: 0;
+    @include tablet {
+      display: flex;
+      flex-direction: row;
+    }
 
     &_link {
       text-decoration: none;
       margin-left: 20px;
       color: $navbar-link;
+
+      //todo click effect
+      //todo animation effect
     }
   }
 }
