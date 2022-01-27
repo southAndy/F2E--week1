b5db2527caca8b603577f29cf605cfba0f50fd65
AT <zatchbell.1206@gmail.com>
2022-01-27 11:11:26 +0800

    feat(Search):adding mobile layout

--- f2e-week1/src/components/Serach.vue
+++ f2e-week1/src/components/Serach.vue
@@ -11,7 +11,9 @@
       placeholder="你想去哪？請輸入關鍵字"
     />
     <a href="##" class="serach_click">
-      <img src="@/assets/image/search.svg" alt="放大鏡" />
+      <div>
+        <img src="@/assets/image/search.svg" alt="放大鏡" />
+      </div>
       <span>搜尋</span>
     </a>
   </div>
@@ -22,18 +24,48 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$primary-color: #7f977b;
 %serach_input {
-  width: 90%;
-  height: 50px;
-  padding: 15px 11px;
+  width: 100%;
+  padding: 15px;
+  box-sizing: border-box;
+
+  margin-bottom: 10px;
+  border-radius: 6px;
+  border: 1px solid #e6e6e6;
 }
 .serach {
+  width: 345px;
+  margin: 0px;
   .select {
     @extend %serach_input;
+    padding: 15px 11px;
+    color: $primary-color;
+    font-weight: 500;
   }
 
   &_input {
     @extend %serach_input;
+    background-color: #f9f9f9;
+    padding: 15px;
+    font-weight: 400;
+    font-size: 15px;
+  }
+  &_click {
+    display: flex;
+    justify-content: center;
+    background-color: $primary-color;
+    @extend %serach_input;
+    text-decoration: none;
+
+    img {
+      margin-right: 10px;
+    }
+    span {
+      color: white;
+      font-weight: 700;
+      align-self: center;
+    }
   }
 }
 </style>
