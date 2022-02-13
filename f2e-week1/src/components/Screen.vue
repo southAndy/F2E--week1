f743207443bf9eab8e2535a84fec99a6c19579af
AT <zatchbell.1206@gmail.com>
2022-02-13 14:42:15 +0800

    feat()adding activities's component

---
+++ f2e-week1/src/components/Screen.vue
@@ -0,0 +1,44 @@
+<template>
+  <router-link
+    class="activities"
+    to="{name: 'Content', params: { id: changeDataType }"
+  >
+    <div class="activities_image"><img src="##" alt="" /></div>
+    <h4 class="activities_title">{{ "title" }}</h4>
+    <span class="activities_location">{{ "地點" }}</span>
+  </router-link>
+</template>
+<script>
+export default {
+  name: "Screen",
+};
+</script>
+<style lang="scss" scoped>
+.activities {
+  display: flex;
+  flex-direction: column;
+  flex: 100%;
+
+  margin-right: 16px;
+
+  &_image {
+    background-color: wheat;
+    width: 225px;
+    height: 160px;
+    border-radius: 20px;
+  }
+  &_title {
+    text-align: start;
+    font-weight: 700;
+    font-size: 18px;
+    color: black;
+    margin: 0;
+  }
+  &_location {
+    text-align: start;
+    font-weight: 400;
+    font-size: 18px;
+    color: black;
+  }
+}
+</style>
