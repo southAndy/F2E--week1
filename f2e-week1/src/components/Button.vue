4bc7c02965ae9d6bec1ba80febc704d849873aa0
AT <zatchbell.1206@gmail.com>
2022-02-16 18:01:24 +0800

    feat(RWD):adding desktop's layout

--- f2e-week1/src/components/Button.vue
+++ f2e-week1/src/components/Button.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="serach_container">
     <!-- 下拉表單樣式優化 -->
-    <select name="請選擇縣市" id="">
+    <select name="請選擇縣市">
       <option value="">請選擇縣市</option>
       <option value="">屏東</option>
       <option value="">高雄</option>
       <option value="">台南</option>
       <option value="">嘉義</option>
     </select>
-    <input type="date" placeholder="請選擇日期" />
+    <input class="serach_time" type="date" placeholder="請選擇日期" />
     <input
       class="serach_input"
       type="text"
@@ -17,7 +17,7 @@
       placeholder="想找有趣的？請輸入搜尋"
     />
     <button class="serach">
-      <div>
+      <div class="serach_icon">
         <img src="@/assets/image/search.svg" alt="放大鏡" />
       </div>
       <span>搜 尋</span>
@@ -31,6 +31,8 @@ export default {
 </script>
 <style lang="scss" scoped>
 @use "../assets/sass/colors.scss";
+@use "../assets/sass/breakpoints.scss";
+
 %serach {
   padding: 15px;
   border: 1px solid;
@@ -40,34 +42,73 @@ export default {
 .serach_container {
   display: flex;
   flex-direction: column;
+  @include breakpoints.desktop {
+    flex-direction: row;
+  }
+  .serach_time {
+    @include breakpoints.desktop {
+      // flex-direction: row;
+      display: none;
+    }
+  }
 
   select {
     width: 100%;
     @extend %serach;
-    border: 1px solid;
+    border: 1px solid #e6e6e6;
+    @include breakpoints.desktop {
+      // flex-direction: row;
+      // display: none;
+      flex: 0.5;
+
+      margin-right: 15px;
+    }
   }
 
   input {
     @extend %serach;
     border: 1px solid;
   }
-  .search_input {
+  .serach_input {
     background-color: #f9f9f9;
+    border: 1px solid #e6e6e6;
+    @include breakpoints.desktop {
+      // flex-direction: row;
+      // display: none;
+      flex: 1.5;
+
+      margin-right: 15px;
+    }
   }
   .serach {
     display: flex;
     justify-content: center;
-    padding: 15px;
+    padding: 0 15px;
     // sass 變數值引入用：sass引入名+變數名
     background-color: colors.$primary-color;
 
     border-radius: 5px;
     border: 1px;
+    @include breakpoints.desktop {
+      // flex-direction: row;
+      // display: none;
+      flex: 0.5;
+      height: 56px;
+    }
     span {
       align-self: center;
       color: white;
       font-size: 20px;
       font-weight: 700;
+    }
+  }
+  .serach_icon {
+    @include breakpoints.desktop {
+      // flex-direction: row;
+      // display: none;
+      // flex: 0.5;
+      // height: 56px;
+      align-self: center;
     }
   }
 }
