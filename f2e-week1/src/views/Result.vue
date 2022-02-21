e7d269454cbb8a4c2878c6474eadf21f6aecf421
AT <zatchbell.1206@gmail.com>
2022-02-21 12:22:52 +0800

    feat(PWD):adding desktop's layout

--- f2e-week1/src/views/Result.vue
+++ f2e-week1/src/views/Result.vue
@@ -1,32 +1,54 @@
 <template>
-  <Button />
-  <div class="serach_amount">
-    <h2>搜尋結果</h2>
-    <span>共有{{ 3 }}筆結果</span>
-  </div>
-  <router-link :to="{ name: 'Content' }">
-    <div class="result" v-for="amount in dataAmounts" :key="amount">
-      <div class="result_image">
-        <img src="../assets/image/RestaurantPicture＿mobile.png" alt="" />
-      </div>
-      <p class="result_name">{{ "景點名稱" }}</p>
-      <p class="result_place">{{ "景點位置" }}</p>
+  <div class="search_all">
+    <Breadcrumb />
+    <Button />
+    <div class="serach_amount">
+      <h2>搜尋結果</h2>
+      <span>共有{{ dataAmounts }}筆結果</span>
     </div>
-  </router-link>
+    <router-link :to="{ name: 'Content' }" class="result_container">
+      <div class="result" v-for="amount in dataAmounts" :key="amount">
+        <div class="result_image">
+          <img
+            class="mobile"
+            src="../assets/image/RestaurantPicture＿mobile.png"
+            alt="景點圖片"
+          />
+          <img
+            class="desktop"
+            src="../assets/image/RestaurantPicture.png"
+            alt="景點圖片"
+          />
+        </div>
+        <p class="result_name">{{ "景點名稱" }}</p>
+        <div class="result_location">
+          <img src="@/assets/image/Vector.png" alt="座標圖示" />
+          <p>{{ "景點位置" }}</p>
+        </div>
+      </div>
+    </router-link>
+  </div>
 </template>
 <script>
 import Button from "@//components/Button.vue";
+import Breadcrumb from "../components/Breadcrumb.vue";
 export default {
   name: "Result",
-  components: { Button },
+  components: { Button, Breadcrumb },
   data() {
     return {
-      dataAmounts: 3,
+      dataAmounts: 30,
     };
   },
 };
 </script>
 <style lang="scss" scoped>
+@use "../assets/sass/breakpoints.scss";
+@use "../assets/sass/reset.scss";
+
+.search_all {
+  padding: 0 45px;
+}
 p {
   margin: 0;
 }
@@ -46,20 +68,64 @@ p {
     margin-left: 10px;
   }
 }
-.result {
+.result_container {
   display: flex;
-  &_image {
-    height: 100%;
-    width: 100%;
-    flex: 100%;
+  flex-direction: column;
+  margin-top: 12px;
+  @include breakpoints.desktop {
+    flex-direction: row;
+    flex-wrap: wrap;
   }
-  &_name {
-    font-size: 18px;
-    font-weight: 700;
-  }
-  &_place {
-    font-size: 15px;
-    font-weight: 400;
+  .result {
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    @include breakpoints.desktop {
+      margin-right: 30px;
+      margin-bottom: 30px;
+    }
+    &_image {
+      height: 100%;
+      width: 100%;
+      flex: 100%;
+    }
+    &_name {
+      font-size: 18px;
+      font-weight: 700;
+      color: #2f2f2f;
+      margin-top: 10px;
+
+      @include breakpoints.desktop {
+        font-size: 22px;
+      }
+    }
+    &_location {
+      display: flex;
+      height: 16px;
+      img {
+        height: 100%;
+        margin-right: 5px;
+      }
+      p {
+        font-size: 15px;
+        font-weight: 400;
+        color: #646464;
+      }
+    }
+    &_image {
+      .mobile {
+        display: block;
+        @include breakpoints.desktop {
+          display: none;
+        }
+      }
+      .desktop {
+        display: none;
+        @include breakpoints.desktop {
+          display: block;
+        }
+      }
+    }
   }
 }
 </style>
