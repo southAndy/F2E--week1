f23e141e7303bec6baaf9d2acd2f5b78af562c4c
Andy <andy@AndydeMacBook-Pro.local>
2022-03-30 15:00:54 +0800

    feat(Vuex):store components data and methods to vuex

--- f2e-week1/src/store/restaurant/index.js
+++ f2e-week1/src/store/restaurant/index.js
@@ -1,6 +1,61 @@
-export const restaurantData = {
-  state: {},
+import API from "@/service/getAPI";
+
+export const restaurantDatas = {
+  namescaped: true,
+  state: {
+    restaurantData: [
+      {
+        name: "地方特產",
+        image: require("@/assets/image/Rectangle 93.svg"),
+      },
+      {
+        name: "中式美食",
+        image: require("@/assets/image/unsplash_qDZ-Xd8dX6w.png"),
+      },
+      {
+        name: "甜點冰品",
+        image: require("@/assets/image/unsplash_pn5c-CLWGzY.svg"),
+      },
+      {
+        name: "異國料理",
+        image: require("@/assets/image/unsplash_qDBbM9Erwo4.svg"),
+      },
+      {
+        name: "伴手禮",
+        image: require("@/assets/image/unsplash_qDZ-Xd8dX6w.png"),
+      },
+      {
+        name: "素食",
+        image: require("@/assets/image/unsplash_qDZ-Xd8dX6w.svg"),
+      },
+    ],
+    apiData: null,
+    //透過類別(class)篩選的資料
+    apiDataByClass: null,
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    storeAPI(state, apiData) {
+      console.log("取到的資料為", apiData);
+      state.apiDataByClass = apiData;
+      console.log("state-apiDataByClass值為", state.apiDataByClass);
+      // return state.apiDataByClass;
+    },
+  },
+  actions: {
+    async sendData({ commit, state }, className) {
+      console.log("此次搜尋類別為：", className);
+
+      let apiData = await API.restaurant
+        .getDataByClass(className)
+        .then((response) => {
+          return response.data;
+        });
+      //將值存入state
+      commit("storeAPI", apiData);
+
+      //把值傳出去
+      return state.apiDataByClass;
+    },
+  },
 };
--- f2e-week1/src/views/Restaurant.vue
+++ f2e-week1/src/views/Restaurant.vue
@@ -7,11 +7,11 @@
       <div class="topic_container">
         <h3 class="serach_theme">熱門主題</h3>
         <Topic
-          v-for="topic in festivalData"
+          v-for="topic in getRestaurantName"
           :key="topic"
           :festivalList="topic"
           class="topic"
-          @click="getDataByClass(topic.name)"
+          @click="getAPI(topic.name)"
         />
       </div>
     </div>
@@ -22,8 +22,7 @@ import Topic from "@/components/Topic.vue";
 import Button from "@/components/Button.vue";
 import Breadcrumb from "@/components/Breadcrumb.vue";
 // import Swiper from "@/components/Swiper.vue";
-
-import API from "@/service/getAPI";
+// import API from "@/service/getAPI";
 
 export default {
   name: "Restaurant",
@@ -35,54 +34,44 @@ export default {
   },
   data() {
     return {
-      festivalData: [
-        // ~ok
-        {
-          name: "地方特產",
-          image: require("../assets/image/Rectangle 93.svg"),
-        },
-        {
-          name: "中式美食",
-          image: require("../assets/image/unsplash_qDZ-Xd8dX6w.png"),
-        },
-        {
-          name: "甜點冰品",
-          image: require("../assets/image/unsplash_pn5c-CLWGzY.svg"),
-        },
-        {
-          name: "異國料理",
-          image: require("../assets/image/unsplash_qDBbM9Erwo4.svg"),
-        },
-        {
-          name: "伴手禮",
-          image: require("../assets/image/unsplash_qDZ-Xd8dX6w.png"),
-        },
-        {
-          name: "素食",
-          image: require("../assets/image/unsplash_qDZ-Xd8dX6w.svg"),
-        },
-      ],
       apiDataByClass: null,
     };
   },
+  computed: {
+    getRestaurantName() {
+      //取得餐飲的資料
+      return this.$store.state.restaurantDatas.restaurantData;
+    },
+  },
   methods: {
-    async getDataByClass(className) {
-      console.log("此次搜尋類別為：",className);
-      await API.restaurant.getDataByClass(className).then((response) => {
-        return (this.apiDataByClass = response.data);
-      });
+    async getAPI(className) {
+      //沒加上async的話，同步程式碼會先執行，造成取不到資料
+      this.apiDataByClass = await this.$store.dispatch("sendData", className);
       console.log(this.apiDataByClass);
+      this.changeRouter(this.apiDataByClass);
+    },
+    changeRouter(apiData) {
       this.$router.push({
         name: "Result",
         params: {
-          id: this.apiDataByClass[0].RestaurantID,
-          type: this.apiDataByClass[1].Class,
+          id: apiData[0].RestaurantID,
+          type: apiData[1].Class,
         },
         query: {
-          type: this.apiDataByClass[1].Class,
+          type: apiData[1].Class,
         },
       });
     },
+    // 原本在元件發API
+    // async getDataByClass(className) {
+    //   console.log("此次搜尋類別為：", className);
+    //   await API.restaurant.getDataByClass(className).then((response) => {
+    //     return (this.apiDataByClass = response.data);
+    //   });
+    //   console.log("取得資料", this.apiDataByClass);
+    //   //將資料傳入函式執行
+    //   this.changeRouter(this.apiDataByClass);
+    // },
   },
 };
 </script>
@@ -91,9 +80,8 @@ export default {
 .restaurant {
   padding: 0 25px;
 
-  @include breakpoints.desktop{
+  @include breakpoints.desktop {
     padding: 0 45px;
-
   }
 
   .serach {
