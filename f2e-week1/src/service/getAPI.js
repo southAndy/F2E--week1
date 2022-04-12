7f4dc5c14fe647f24344e21dca261d4354965246
Andy <andy@AndydeMacBook-Pro.local>
2022-04-12 17:51:49 +0800

    feat(v1.5):adding the function of keyword,city's API

--- f2e-week1/src/service/getAPI.js
+++ f2e-week1/src/service/getAPI.js
@@ -14,39 +14,54 @@ const touristAPI = axios.create({
 
 export default {
   getScenicSpotAPI() {
-    return touristAPI.get("/ScenicSpot?%24top=300&%24format=JSON");
+    return touristAPI.get("/ScenicSpot?%24top=500&%24format=JSON");
   },
   getActivitiesAPI() {
-    return touristAPI.get("/Activity?%24top=300&%24format=JSON");
+    return touristAPI.get("/Activity?%24top=500&%24format=JSON");
   },
   getRestaurantAPI() {
-    return touristAPI.get("/Restaurant?%24top=300&%24format=JSON");
+    return touristAPI.get("/Restaurant?%24top=500&%24format=JSON");
   },
+  //todo 重新設計觸發API函式：https://motc-ptx-api-documentation.gitbook.io/motc-ptx-api-documentation/api-te-se/odata
   scenicSpot: {
     getDataByClass(className = "") {
       let ASCIIClassName = encodeURI(className);
       console.log("ScenicSpot", ASCIIClassName);
 
-      //axios官方物件寫法
+      //todo 進階寫法：axios官方物件寫法
+      //原始寫法
+      // return touristAPI.get(
+      //   `ScenicSpot?%24filter=contains(Class1%2C%20%27${ASCIIClassName}%27)&%24top=500&%24format=JSON`
+      // );
       return touristAPI.get("ScenicSpot", {
         params: {
           $filter: `contains(Class1, '${className}')`,
-          $top: 300,
+          $top: 500,
           $format: "JSON",
         },
       });
-      //原始寫法
-      // return touristAPI.get(
-      //   `ScenicSpot?%24filter=contains(Class1%2C%20%27${ASCIIClassName}%27)&%24top=300&%24format=JSON`
-      // );
+    },
+    getFilteredDatas(city = "/", keyword = "") {
+      let ASCIIedKeyword = encodeURI(keyword);
+
+      return touristAPI.get(
+        `ScenicSpot/${city}?%24filter=contains(DescriptionDetail%2C%27${ASCIIedKeyword}%27)&%24top=500&%24format=JSON`
+      );
     },
   },
   activities: {
     getDataByClass(className = "") {
       let ASCIIClassName = encodeURI(className);
-      console.log(ASCIIClassName);
+      console.log(`將${className}轉換為${ASCIIClassName}`);
       return touristAPI.get(
-        `Activity?%24filter=contains(Class1%2C%20%27${ASCIIClassName}%27)&%24top=300&%24format=JSON`
+        `Activity?%24filter=contains(Class1%2C%20%27${ASCIIClassName}%27)&%24top=500&%24format=JSON`
+      );
+    },
+    getFilteredDatas(city = "/", keyword = "") {
+      let ASCIIedKeyword = encodeURI(keyword);
+
+      return touristAPI.get(
+        `Activity/${city}?%24filter=contains(Description%2C%27${ASCIIedKeyword}%27)&%24top=500&health=false&%24format=JSON`
       );
     },
   },
@@ -55,7 +70,14 @@ export default {
       let ASCIIClassName = encodeURI(className);
       console.log(ASCIIClassName);
       return touristAPI.get(
-        `Restaurant?%24filter=contains(Class%2C%20%27${ASCIIClassName}%27)&%24top=300&%24format=JSON`
+        `Restaurant?%24filter=contains(Class%2C%20%27${ASCIIClassName}%27)&%24top=500&%24format=JSON`
+      );
+    },
+    getFilteredDatas(city = "/", keyword = "") {
+      let ASCIIedKeyword = encodeURI(keyword);
+
+      return touristAPI.get(
+        `Restaurant/${city}?%24filter=contains(Description%2C%27${ASCIIedKeyword}%27)&%24top=500&health=false&%24format=JSON`
       );
     },
   },
