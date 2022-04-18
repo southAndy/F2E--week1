1ae3cf09549528c0884226deb1a6ba89838c4054
Andy <andy@AndydeMacBook-Pro.local>
2022-04-18 16:52:25 +0800

    fix(v1.5):fixing wrong folder issue

---
+++ src/components/Loading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loading skeleton">
+    <div class="skeleton"></div>
+    <div class="skeleton"></div>
+    <div class="skeleton"></div>
+    <div class="skeleton"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Loading",
+};
+</script>
+<style lang="scss" scoped>
+@use "../assets/sass/loading.scss";
+
+.loading {
+  height: 300px;
+  width: 100%;
+  padding: 300px;
+}
+.skeleton {
+  @extend %skeleton-loading;
+}
+div.skeleton {
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+div {
+  margin-bottom: 20px;
+}
+</style>
