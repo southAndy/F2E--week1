dab56498adea509a0d4fd1dc091b2c396e1dcae8
southAndy <zatchbell.1206@gmail.com>
2024-12-04 15:49:13 +0800

    feat(ts): modify vue's typescript compile

--- src/types/shim-vue.d.ts
+++ src/types/shim-vue.d.ts
@@ -1,5 +1,12 @@
+import '@/types/api.d.ts'
+import '@/types/global.d.ts'
+
+
 declare module '*.vue' {
     import { DefineComponent } from 'vue';
     const component: DefineComponent<{}, {}, any>;
     export default component;
   }
+
+
+
