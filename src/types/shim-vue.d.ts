fedf6596e07e32febf4cd8eb22caad787a85ae04
southAndy <zatchbell.1206@gmail.com>
2024-12-09 16:51:55 +0800

    feat(type):add vue sfc's declaration

--- src/types/shim-vue.d.ts
+++ src/types/shim-vue.d.ts
@@ -1,7 +1,7 @@
 import '@/types/api.d.ts'
 import '@/types/global.d.ts'
 
-
+// for optionAPI's setting
 declare module '*.vue' {
     import { DefineComponent } from 'vue';
     const component: DefineComponent<{}, {}, any>;
